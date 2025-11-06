1ac83359fb4f22722efd570da0b9c5db2c58a4c2
Alexsanei <senai.alexsander@gmail.com>
2025-11-06 13:47:25 -0300

    Correção

--- model/src/main/java/model/Aluno.java
+++ model/src/main/java/model/Aluno.java
@@ -13,7 +13,7 @@ public class Aluno {
     double media;
     
     public double calcularMedia(double nota1, double nota2, double nota3)throws IllegalArgumentException{ 
-        if(nota1 < 0 || nota1 >10 || nota2 < 0 || nota2 >10 || nota3 < 0 || nota3 >10)
+        if( (nota1 < 0 || nota1 >10) || (nota2 < 0 || nota2 >10) || (nota3 < 0 || nota3 >10))
             throw new IllegalArgumentException (" Sua nota deve ser maior que 0 e menor que 10 ");
     
         media = (nota1+nota2+nota3) / 3;
